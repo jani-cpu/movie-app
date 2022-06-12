5ed5f94ad51e0ea3a054843fd7b3b5a432edaa41
jirigi <Jani.Irigi@gilead.com>
2022-06-12 13:22:26 +0530

    userProfile code

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import userImage from "../images/user-profile-svgrepo-com.svg";
 import { Link } from "react-router-dom";
+import UserProfile from "../userProfile/UserProfile";
 
 function Header() {
+  const [showModal, setShowModal] = useState(false);
+  const userShowHandler = () => {
+    setShowModal(!showModal);
+  };
   return (
     <div className='header'>
       <Link to='/'>
-        <h2 className='appTitle'>MovieApp</h2>
+        <div className='headerElement'>
+          <img
+            className='logoImage'
+            src='https://play-lh.googleusercontent.com/TBRwjS_qfJCSj1m7zZB93FnpJM5fSpMA_wUlFDLxWAb45T9RmwBvQd5cWR5viJJOhkI'
+            alt='MovieApp'
+          />
+          <h2 className='appTitle'>MovieApp</h2>
+        </div>
       </Link>
-      <img className='profileImage' src={userImage} alt='MovieApp' />
+      <img
+        onClick={userShowHandler}
+        className='profileImage'
+        src={userImage}
+        alt='MovieApp'
+      />
+      {showModal && <UserProfile />}
     </div>
   );
 }
---
+++ src/components/userProfile/UserProfile.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./UserProfile.css";
+
+function UserProfile() {
+  return (
+    <div className='userProfileContainer'>
+      <p>About Me</p>
+      <p>Sign Out</p>
+      <p>My Watchlist</p>
+    </div>
+  );
+}
+
+export default UserProfile;
